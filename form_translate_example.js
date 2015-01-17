d6ebb52895ff3d201af6a07691ba183444fe61f7
Mark Tomm <marktomm90@gmail.com>
2015-01-17 11:31:57 +0200

    working form example with no translations

--- form_translate_example.js
+++ form_translate_example.js
@@ -1,23 +1,73 @@
+Forms = new Meteor.Collection('forms');
+
+/*
+  Mesosphere and server method should both run in shared space for dual client/server
+  validation to work.
+ */
+Mesosphere({
+    name: 'signupForm',
+    method: 'signupUser',
+    fields: {
+        name: {
+            required: true,
+            format: 'alphanumeric',
+            message: 'Please enter a valid name'
+        },
+        phone: {
+            required: true,
+            format: 'phone',
+            rules: {
+                minLength: 2,
+                maxLength: 30,
+            },
+            message: "Phone must be between 2 and 30 numbers"
+        },
+        email: {
+            required: true,
+            format: 'email',
+            message: "Invalid e-mail"
+        }
+    }
+
+});
+
+Meteor.methods({
+  signupUser: function (rawData) {
+    Mesosphere.signupForm.validate(rawData, function(errors, formData){
+        if(!errors){
+            console.log("No Errors Found");
+            Meteor.call("addFormToDb", formData);
+            //Do what you want with the validated data.
+        }else{
+            _(errors).each( function( value, key ) {
+              console.log(key+": "+value.message);
+            });
+        }
+    });
+  },
+  addFormToDb: function (formData) {
+    Forms.insert({
+      createdAt: new Date(),
+      name: formData.name,
+      phone: formData.phone,
+      email: formData.email
+    });
+  },
+});
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+  Meteor.subscribe('forms');
+  
+  Template.body.helpers({
+    getFormEntriesFromDb: function () {
+      return Forms.find({});
     }
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
-}
+};
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.publish('forms', function() {
+    return Forms.find({});
   });
-}
+};
