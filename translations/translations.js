cc0305f1e97b5be7ebfcde91ae58b5b1681bc251
Mark Tomm <marktomm90@gmail.com>
2015-01-24 13:13:48 +0200

    translated eerything using Nemo64:meteor-translator package

--- translations/translations.js
+++ translations/translations.js
@@ -8,10 +8,31 @@ if(Meteor.isClient) {
   
   Template.langNav.events({
     "click #est": function() {
-      Translator.setLanguage(["ee"]);
+      translate("ee");
     },
     "click #eng": function() {
-      Translator.setLanguage(["en"]);
+      translate("en");
     }
   });
+  
+  Template.body.helpers({
+    trans: FrontLang.createHelper()
+  });
+  
+  Template.exampleForm.helpers({
+    trans: FrontLang.createHelper()
+  });
+}
+
+function translate(lang)
+{
+  Translator.setLanguage([lang]);
+  formTranslate();
+}
+
+function formTranslate()
+{
+  Mesosphere.signupForm.fields.name.message = FrontLang.get("form.error.name");
+  Mesosphere.signupForm.fields.phone.message = FrontLang.get("form.error.phone");
+  Mesosphere.signupForm.fields.email.message = FrontLang.get("form.error.email");
 }
